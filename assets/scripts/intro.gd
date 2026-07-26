47a8f8ed322c2311cc7c59c3b07cb035f2fe7536
sajiddhossain <sajid.hossain2009@gmail.com>
2026-07-26 10:30:37 +0200

    restore lowercase paths and drop duplicate music spawner

--- assets/scripts/intro.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var music_scene = preload("res://Assets/scenes/music.tscn").instantiate()
-
-func _ready() -> void:
-	await get_tree().create_timer(5.0).timeout
-	add_child(music_scene)
